57f1e269fd8a68e27518f49357f25bca4b89e7f8
Najeeb Aqel <najeebmms@hotmail.com>
2019-04-24 19:16:07 +0300

    ReadFromTheWebCam

--- main.py
+++ main.py
@@ -18,115 +18,137 @@ def findContour(image):
 
 
 
-# load input and convert it to gray scale 
-imageA = cv.imread("image3.jpg")
-grayA = cv.cvtColor(imageA, cv.COLOR_BGR2GRAY)
-cv.imwrite('grayimage.jpg', grayA)
+cap = cv.VideoCapture(0)
 
-#thresholding 
-ret,thresh1 = cv.threshold(grayA,200,255,cv.THRESH_BINARY_INV)
-cv.imwrite('thresh.jpg', thresh1)
-cv.imshow("threshold",thresh1)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-#Finding Contours of the sign
-contoursN, count = findContour(thresh1)
-#printing the approximation
-# for  i in range(0,count):
-#         print("contour"+str(i)+" size ="+str(len(contoursN[i]))) 
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-# Triangle Detection
-if len(contoursN[0]) == 3:
-    print("The sign has a shape of a Triangle.")
-    if count == 2:
-        point1 = contoursN[0][1][0][1]
-        point2 = contoursN[0][2][0][1]
-        differ = abs(point1 - point2)
-        if differ < 11:
-            print("dangerous descent.")
-        else:
-            print("Give Way")
-    elif count == 3:
-        print("Bumby Road")
-    elif count == 5:
-        print("Traffic Lights Ahead.")
-         
-#Circle Detection
-elif len(contoursN[0]) > 6:
-    print("The sign has a shape of a Circle or Octagon.")
-    #print("",len(contoursN)-2)
-    if count == 1:
-        print("No Parking.")
-    elif count == 3:
-        print("No Entry.")
-    elif len(contoursN[count-1]) > 10 :
-        print("Stop.")
-    elif count == 5:
-        print("End Speed Limit.")
-    else:
-        point1 = contoursN[count-1][0][0][0]
-        point2 = contoursN[count-1][0][0][1]
-        if point1 > point2:
-            point1 = contoursN[count-1][0][0][0]
-            point2 = contoursN[count-1][1][0][0]
-            dif = point2 - point1
-            if dif > 5:
-                print("Go Straight.")
+    # Display the resulting frame
+    cv.imshow('frame',gray)
+
+    # load input and convert it to gray scale 
+    imageA = cv.imread("image3.jpg")
+    grayA = gray                    #cv.cvtColor(imageA, cv.COLOR_BGR2GRAY)
+    cv.imwrite('grayimage.jpg', grayA)
+
+    #thresholding 
+    ret,thresh1 = cv.threshold(grayA,200,255,cv.THRESH_BINARY_INV)
+    cv.imwrite('thresh.jpg', thresh1)
+    cv.imshow("threshold",thresh1)
+
+    #Finding Contours of the sign
+    contoursN, count = findContour(thresh1)
+    #printing the approximation
+    # for  i in range(0,count):
+    #         print("contour"+str(i)+" size ="+str(len(contoursN[i]))) 
+
+    # Triangle Detection
+    if len(contoursN[0]) == 3:
+        print("The sign has a shape of a Triangle.")
+        if count == 2:
+            point1 = contoursN[0][1][0][1]
+            point2 = contoursN[0][2][0][1]
+            differ = abs(point1 - point2)
+            if differ < 11:
+                print("dangerous descent.")
             else:
-                print("Go Right.")
-        elif point1 < point2:
-            print("Go Left.")
+                print("Give Way")
+        elif count == 3:
+            print("Bumby Road")
+        elif count == 5:
+            print("Traffic Lights Ahead.")
             
-#Rectangle Detection
-elif len(contoursN[0]) == 4:
-    print("The sign has a shape of a Rectangle.")
-    hsv = cv.cvtColor(imageA, cv.COLOR_BGR2HSV)
-    # define range of blue color in HSV
-    lower_blue = np.array([97,50,50])
-    upper_blue = np.array([130,255,255])
-    # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
-    cv.imshow("blue",mask)
-    contoursN1, count1 = findContour(mask)
-    if count1 > 0:
-        print("This has a blue color.")
-        print("Freeway Entry.")
-    else:
-        lower_yellow = np.array([22,50,50])
-        upper_yellow = np.array([32,255,255])
-        # Threshold the HSV image to get only blyellow colors
-        mask = cv.inRange(hsv, lower_yellow, upper_yellow)
-        cv.imshow("yellow",mask)
+    #Circle Detection
+    elif len(contoursN[0]) > 6:
+        print("The sign has a shape of a Circle or Octagon.")
+        #print("",len(contoursN)-2)
+        if count == 1:
+            print("No Parking.")
+        elif count == 3:
+            print("No Entry.")
+        elif len(contoursN[count-1]) > 10 :
+            print("Stop.")
+        elif count == 5:
+            print("End Speed Limit.")
+        else:
+            point1 = contoursN[count-1][0][0][0]
+            point2 = contoursN[count-1][0][0][1]
+            if point1 > point2:
+                point1 = contoursN[count-1][0][0][0]
+                point2 = contoursN[count-1][1][0][0]
+                dif = point2 - point1
+                if dif > 5:
+                    print("Go Straight.")
+                else:
+                    print("Go Right.")
+            elif point1 < point2:
+                print("Go Left.")
+                
+    #Rectangle Detection
+    elif len(contoursN[0]) == 4:
+        print("The sign has a shape of a Rectangle.")
+        hsv = cv.cvtColor(imageA, cv.COLOR_BGR2HSV)
+        # define range of blue color in HSV
+        lower_blue = np.array([97,50,50])
+        upper_blue = np.array([130,255,255])
+        # Threshold the HSV image to get only blue colors
+        mask = cv.inRange(hsv, lower_blue, upper_blue)
+        cv.imshow("blue",mask)
         contoursN1, count1 = findContour(mask)
         if count1 > 0:
-            print("This has a yellow color.")
-            print("Exit.")
+            print("This has a blue color.")
+            print("Freeway Entry.")
         else:
-            lower_green = np.array([70,50,50])
-            upper_green = np.array([100,255,255])
-
-            # Threshold the HSV image to get only green colors
-            mask = cv.inRange(hsv, lower_green, upper_green)
-            cv.imshow("green",mask)
+            lower_yellow = np.array([22,50,50])
+            upper_yellow = np.array([32,255,255])
+            # Threshold the HSV image to get only blyellow colors
+            mask = cv.inRange(hsv, lower_yellow, upper_yellow)
+            cv.imshow("yellow",mask)
             contoursN1, count1 = findContour(mask)
             if count1 > 0:
-                print ("This has a green color.")
-                print("Major Road Sign.")
-                           
-#Pentagon Detection
-elif len(contoursN[0]) == 5:
-    print("The sign has a shape of a Pentagon.")
-    hsv = cv.cvtColor(imageA, cv.COLOR_BGR2HSV)
-    # define range of blue color in HSV
-    lower_brown = np.array([0,50,50])
-    upper_brown= np.array([30,255,255])
-    # Threshold the HSV image to get only brown colors
-    mask = cv.inRange(hsv, lower_brown, upper_brown)
-    cv.imshow("brown",mask)
-    contoursN1, count1 = findContour(mask)
-    if count1 > 0:
-        print ("this has a brown color.")
-        print("Tourist Destination.")
-    else:
-        print("Local Destination.")
+                print("This has a yellow color.")
+                print("Exit.")
+            else:
+                lower_green = np.array([70,50,50])
+                upper_green = np.array([100,255,255])
 
-cv.waitKey(0)
+                # Threshold the HSV image to get only green colors
+                mask = cv.inRange(hsv, lower_green, upper_green)
+                cv.imshow("green",mask)
+                contoursN1, count1 = findContour(mask)
+                if count1 > 0:
+                    print ("This has a green color.")
+                    print("Major Road Sign.")
+                            
+    #Pentagon Detection
+    elif len(contoursN[0]) == 5:
+        print("The sign has a shape of a Pentagon.")
+        hsv = cv.cvtColor(imageA, cv.COLOR_BGR2HSV)
+        # define range of blue color in HSV
+        lower_brown = np.array([0,50,50])
+        upper_brown= np.array([30,255,255])
+        # Threshold the HSV image to get only brown colors
+        mask = cv.inRange(hsv, lower_brown, upper_brown)
+        cv.imshow("brown",mask)
+        contoursN1, count1 = findContour(mask)
+        if count1 > 0:
+            print ("this has a brown color.")
+            print("Tourist Destination.")
+        else:
+            print("Local Destination.")
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
+
+
+
+
+
+
